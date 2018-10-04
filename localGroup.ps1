2af955c3c3debe9e7948610d4186b86a75ad6f95
Varunaditya Jadwal <vjadwal@genevatrading.com>
2018-10-04 13:20:46 -0500

    initial commit

---
+++ localGroup.ps1
@@ -0,0 +1,23 @@
+#A PS script that reads the host names and group names from a text file and lists 
+#all the members on that host name from those groups 
+#Author: Varunaditya Jadwal
+
+$hostNames = Get-content "C:\Users\vjadwal\Documents\successfulConnections.txt"
+$localGroups = Get-content "C:\Users\vjadwal\Documents\groupsList.txt"
+$outputFile = "C:\Users\vjadwal\Documents\groupMembers.txt"
+$dataToBeWritten = @()
+foreach ($hostName in $hostNames){
+	$dataToBeWritten += 'Hostname: ' + $hostName
+	foreach ($localGroup in $localGroups){
+		$dataToBeWritten += 'Local Group: ' + $localGroups
+		$groupMembers = Invoke-Command -Computer $hostName -ScriptBlock {
+			$group = [ADSI]("WinNT://$env:COMPUTERNAME/$($args[0]),group")
+			$group.PSBase.Invoke('Members') | % {
+			$_.GetType().InvokeMember('Name', 'GetProperty', $null, $_, $null)
+			}
+		} -ArgumentList $localGroups -EA SilentlyContinue
+		$dataToBeWritten += 'Members: ' + $groupMembers
+		$dataToBeWritten += ''
+	$dataToBeWritten += '------------------------------------------------'
+}
+$dataToBeWritten > $outputFile
